b48ae9ee39873b891c980f50296cb781cb141364
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-06-13 15:18:14 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
 // Code MovieReviews Here
+
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+
+    const renderReviews = () => {
+        return reviews.map(review => {
+            return <li key={review.display_title}>{review.display_title}: {review.summary_short}</li>
+        })
+    }
+
+    return(
+        <div className='review-list'>
+            <ul>
+            {renderReviews()}
+            </ul>
+        </div>
+    )
+
+}
+
+export default MovieReviews
